b7e2e8c39c1d1d514a55f375579fc48fe713d36b
LAPTOP-7GTIMF9A\adnan <adnan6official@gmail.com>
2024-07-19 16:27:32 +0500

    created structure of frontend

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <button className="btn btn-primary">One</button>
+      <button className="btn btn-secondary">Two</button>
+      <button className="btn btn-accent btn-outline">Three</button>
+    </div>
+  );
+};
+
+export default About;
